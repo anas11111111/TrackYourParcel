3a0294f459c895c57550508fdec54c44f9d32492
anas11111111 <160204021@aust.edu>
2022-02-02 22:42:41 +0600

    commit

--- services/userService.js
+++ services/userService.js
@@ -24,15 +24,23 @@ export const update = async (user) => {
 }
 
 export const deleteById = async (id) => {
-   // let id = ObjectId.fromString( myObjectIdString );
+    // let id = ObjectId.fromString( myObjectIdString);
     const User = models.User;
-    console.log("service"+id);
-    let model = await User.findById(id);
-    console.log("model",model);
-    if (model){
-        let result = await User.deleteOne({ _id: id });
-        return result;
+    console.log("service" + id);
+    if (id.length == 24) {
+        let model = await User.findById(id);
+        console.log("model", model);
+        if (model) {
+            let result = await User.deleteOne({ _id: id });
+            return result;
+        }
+        return new Error('User not found by the id ' + id);
     }
-    return new Error('User not found by the id' + id);
+    else {
+        return new Error('User not found by the id ' + id);
+
+    }
+
+
 
 }
